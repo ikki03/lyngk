7e85e3bdd7c5a6f9c13008687ba43f997fb0c04d
ikki03 <nico.focqueu0307@gmail.com>
2017-10-02 11:30:23 +0200

    test histoire 2 ok

--- test/test.js
+++ test/test.js
@@ -1,7 +1,20 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
- LyngkTestCase.prototype.testcoorok = function () {
-     var testcase = new Lyngk.Coordinates("A",1);
-     assertFalse(testcase.valid());
- }
+LyngkTestCase.prototype.testcoorok = function () {
+    var testcase = new Lyngk.Coordinates("A", 1);
+    assertFalse(testcase.valid());
+}
+LyngkTestCase.prototype.testhist2 = function () {
+    var compteur = 0;
+    var testcase;
+    for (var c = 65; c <= 74; c++) {
+        for (var l = 1; l <= 9; l++) {
+            testcase = new Lyngk.Coordinates(String.fromCharCode(c), l);
+            if (testcase.valid()) {
+                compteur++
+            }
+        }
+    }
+    assertEquals(compteur, 43);
+}
