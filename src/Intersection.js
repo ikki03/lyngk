b25375270b21ce894a6203a20d06ea7761f7cbda
ikki03 <nico.focqueu0307@gmail.com>
2017-10-09 11:03:02 +0200

    histoire 7 ok

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,5 +2,10 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function () {
+    var state = Lyngk.State.VACANT;
+
+    this.getState=function(){
+        return state;
+    };
 };
