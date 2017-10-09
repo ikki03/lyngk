9c9078b735b1de415c8a989b417d1308e81d11f3
ikki03 <nico.focqueu0307@gmail.com>
2017-10-09 11:41:09 +0200

    histoire 9 fino

--- src/Intersection.js
+++ src/Intersection.js
@@ -5,13 +5,21 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 Lyngk.Intersection = function () {
     var state = Lyngk.State.VACANT;
     var piece;
+
     this.getState=function(){
         return state;
     };
+
     this.pose=function(colo){
-        piece = new Lyngk.Piece(colo);
-        state = Lyngk.State.ONE_PIECE;
+        if(!piece) {
+            piece = new Lyngk.Piece(colo);
+            state = Lyngk.State.ONE_PIECE;
+        }else {
+            piece = new Lyngk.Piece(colo);
+            state = Lyngk.State.STACK;
+        }
     };
+
     this.getColor=function(){
         return piece.getColor();
     };
