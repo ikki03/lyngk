824d2410acf225b479e2452b7ba7307f122d38a7
0328410207 <nico.focqueu0307@gmail.com>
2017-10-12 14:20:24 +0200

    test histoire 10 ok modification pour passer en tableau fini

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,24 +4,25 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function () {
     var state = Lyngk.State.VACANT;
-    var piece;
+    var pieces=[];
 
     this.getState=function(){
         return state;
     };
 
     this.pose=function(colo){
-        if(!piece) {
-            piece = new Lyngk.Piece(colo);
+        if(pieces.length<=0) {
             state = Lyngk.State.ONE_PIECE;
-        }else {
-            piece = new Lyngk.Piece(colo);
+        }else if(pieces.length >  0 && pieces.length < 4){
             state = Lyngk.State.STACK;
+        }else if(pieces.length >= 4){
+            state = Lyngk.State.FULL_STACK;
         }
+        pieces.push(new Lyngk.Piece(colo));
     };
 
     this.getColor=function(){
-        return piece.getColor();
+        return pieces[pieces.length-1].getColor();
     };
 
 
