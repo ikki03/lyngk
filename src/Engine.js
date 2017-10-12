aaac64f2a0cf50683de62b7d204668faba055ef5
0328410207 <nico.focqueu0307@gmail.com>
2017-10-12 14:53:05 +0200

    test histoire 11 fini

--- src/Engine.js
+++ src/Engine.js
@@ -4,4 +4,28 @@
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
 Lyngk.Engine = function () {
+    var tab = [];
+
+    this.init_OnePiece = function()
+    {
+        for (var coor in tab) {
+            if (tab.hasOwnProperty(coor))
+            {
+                tab[coor].poser(Lyngk.Color.IVORY);
+            }
+        }
+    };
+
+    this.full_One_Piece = function()
+    {
+        for (var coor in tab) {
+            if (tab.hasOwnProperty(coor))
+            {
+                if(tab[coor].getState() != Lyngk.State.ONE_PIECE)
+                    return false;
+            }
+        }
+        return true;
+    };
+
 };
