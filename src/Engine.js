4d197ba0c0a332ba326ad096dda5c62b5fea790d
ikki03 <nico.focqueu0307@gmail.com>
2017-10-16 10:07:34 +0200

    test histoire 12 fini

--- src/Engine.js
+++ src/Engine.js
@@ -4,14 +4,22 @@
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
 Lyngk.Engine = function () {
-    var tab = [];
+    var tab = {};
+
+    var init = function()
+    {
+        var coordok = Lyngk.valtab;
+        for(var i = 0; i < coordok.length; i++)
+        {
+            tab[coordok[i]] = new Lyngk.Intersection();
+        }
+    };
 
     this.init_OnePiece = function()
     {
         for (var coor in tab) {
-            if (tab.hasOwnProperty(coor))
-            {
-                tab[coor].poser(Lyngk.Color.IVORY);
+            if (tab.hasOwnProperty(coor)) {
+                tab[coor].pose(Lyngk.Color.IVORY);
             }
         }
     };
@@ -27,5 +35,26 @@ Lyngk.Engine = function () {
         }
         return true;
     };
+    this.init_multi_color = function()
+    {
+        var dispo = [8,8,8,8,8,3];
+        for (var coor in tab) {
+            if (tab.hasOwnProperty(coor))
+            {
+                var randomColor;
+                do{
+                    randomColor = Math.floor(Math.random() * 6);
+                }while(dispo[randomColor] <= 0)
+                dispo[randomColor]--;
+                tab[coor].pose(randomColor);
+            }
+        }
+    };
+
+    this.plateau = function() {
+        return tab;
+    };
+
+    init();
 
 };
